783107a59f1347b95acc5a1c5c9f9900497f9cf8
xiaoyaoyimei <1206902591@qq.com>
2019-03-25 14:13:19 +0800

    create project

---
+++ precache-manifest.cd0a157f16d1aae8258a5478e7daeeb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4649bd53aaee3166f213",
+    "url": "jianshu/static/js/runtime~main.edfd4336.js"
+  },
+  {
+    "revision": "606a4fe91fa3d86f6d59",
+    "url": "jianshu/static/js/main.8d2fdd53.chunk.js"
+  },
+  {
+    "revision": "152f01e4141f2110b53f",
+    "url": "jianshu/static/js/3.544d88c6.chunk.js"
+  },
+  {
+    "revision": "8bd711bf37c24345b15c",
+    "url": "jianshu/static/js/2.2954bf44.chunk.js"
+  },
+  {
+    "revision": "73c9d15439cbb94e192e90917add0dc2",
+    "url": "jianshu/index.html"
+  }
+];
